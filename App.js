d8def63efc58d7c9dc846c32fb8b4d0bfa2672b9
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-25 15:15:49 -0400

    beginning logic for login

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { auth } from './firebase';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
+  const [signedIn, setSignedIn] = useState(false);
+
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      setSignedIn(true);
+    } else {
+      setSignedIn(false);
+    }
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        {signedIn
+        ? (
+          <Text>Signed in</Text>
+        ) : (
+          <Text>Not signed in</Text>
+        )}
+      </NavigationContainer>
     </View>
   );
 }
