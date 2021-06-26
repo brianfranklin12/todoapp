14c659e487a4b18968a788488a543df1b6cbced3
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-26 17:12:26 -0400

    hold down on todo to alert for deletion

--- screens/todos/ViewAll.js
+++ screens/todos/ViewAll.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, View } from "react-native";
+import { Alert, ScrollView, View } from "react-native";
 import { List, Divider, FAB } from "react-native-paper";
 
 import { db, auth } from '../../firebase';
@@ -38,6 +38,29 @@ export default function ViewAll({ navigation }) {
     }
   };
 
+  const handleDelete = async (todo) => {
+    const ref = db.collection('todos').doc(todo.id);
+
+    try {
+      Alert.alert(
+        'Are you sure?',
+        'Are you sure you want to delete?',
+        [
+          {
+            text: 'Cancel',
+            onPress: () => null,
+          },
+          {
+            text: 'Delete',
+            onPress: async () => await ref.delete(),
+          }
+        ],
+      )
+    } catch (err) {
+      console.err(err);
+    }
+  }
+
   return (
     <>
       <ScrollView>
@@ -49,6 +72,7 @@ export default function ViewAll({ navigation }) {
                 onPress={() => handleComplete(todo)}
                 titleStyle={todo.completed
                 ? styles.complete : styles.notComplete}
+                onLongPress={() => handleDelete(todo)}
               />
               <Divider />
             </View>
