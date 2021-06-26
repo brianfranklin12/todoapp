d65d8f6fa3cf1db7a87da497b57220a38d272268
Brian Franklin <brianfranklinmusic@gmail.com>
2021-06-26 15:30:42 -0400

    todo list displays to screen

--- screens/sessions/TodosTab.js
+++ screens/sessions/TodosTab.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+import ViewAll from '../todos/ViewAll';
+
+const Stack = createStackNavigator();
+
 const TodosTab = () => {
   return (
-    <Text>Todos</Text>
+    <>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#29434e'
+          }
+        }}
+      >
+        <Stack.Screen 
+          name="viewAll"
+          component={ViewAll}
+          options={{
+            title: 'Todos',
+            headerTintColor: '#fff',
+          }}
+        />
+      </Stack.Navigator>
+    </>
   )
 }
 export default TodosTab;
---
+++ screens/todos/ViewAll.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { ScrollView, View } from "react-native";
+import { List, Divider } from "react-native-paper";
+
+import { db } from '../../firebase';
+
+
+export default function ViewAll() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const ref = db.collection('todos');
+    ref.onSnapshot(query => {
+      const objs = [];
+      query.forEach(doc => {
+        objs.push({
+          id: doc.id, 
+          ...doc.data(),
+        });
+      });
+      setTodos(objs);
+    });
+  }, [])
+
+  return (
+    <ScrollView>
+      <View>
+        {todos.map(todo => (
+          <View key={todo.id}>
+            <List.Item 
+              title={todo.task}
+            />
+            <Divider />
+          </View>
+        ))}
+      </View>
+    </ScrollView>
+  )
+}
